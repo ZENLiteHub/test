577734318a571b4ba29771df827408b7dc0adb0a
Jerry <ojonuba93@gmail.com>
2024-11-22 10:35:05 +0100

    updates server url

--- static/test.js
+++ static/test.js
@@ -1,5 +1,5 @@
 // Replace with your server URL (relative since served on the same domain)
-const SERVER_URL = "/";
+const SERVER_URL = "http://92.205.25.58:5003";
 
 // Retrieve session_id and token from localStorage
 const sessionId = localStorage.getItem("session_id");
